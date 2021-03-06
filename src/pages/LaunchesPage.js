2876092392d1f6e9d1159fcfc67c76f4fa09e1c1
Paulina <paulina.petrulyte@gmail.com>
2021-03-06 12:07:03 +0200

    check initial fetch, create launch and details components

--- src/pages/LaunchesPage.js
+++ src/pages/LaunchesPage.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import { baseUrl } from "../api/baseUrl";
+import Launch from "../components/Launch";
 
 const LaunchesPage = () => {
+  const { loading, data } = useFetch(`${baseUrl}/launches`);
+
+  if (loading) {
+    return <h2>Loading....</h2>;
+  }
   return (
     <div>
       <h1>Space X Launches Page</h1>
+      <section>
+        {data.map((launch) => {
+          return (
+            <Link to={`/launch/${launch.id}`} key={launch.id}>
+              <Launch name={launch.name} />
+            </Link>
+          );
+        })}
+      </section>
     </div>
   );
 };
